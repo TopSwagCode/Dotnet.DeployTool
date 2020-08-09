5f509fc39cc142199789512ea794e1f4e2c3da24
Joshua Ryder <joshua.ryder@outlook.com>
2020-08-09 11:40:47 +0200

    Going from blazor server to blazor WASM core hosted

---
+++ src/Dotnet.DeployTool.UI/Shared/Chat/ChatClient.cs
@@ -0,0 +1,170 @@
+﻿
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dotnet.DeployTool.UI.Shared.Chat
+{
+    /// <summary>
+    /// Generic client class that interfaces .NET Standard/Blazor with SignalR Javascript client
+    /// </summary>
+    public class ChatClient : IAsyncDisposable
+    {
+        public const string HUBURL = "/ChatHub";
+
+        private readonly string _hubUrl;
+        private HubConnection _hubConnection;
+
+        /// <summary>
+        /// Ctor: create a new client for the given hub URL
+        /// </summary>
+        /// <param name="siteUrl">The base URL for the site, e.g. https://localhost:1234 </param>
+        /// <remarks>
+        /// Changed client to accept just the base server URL so any client can use it, including ConsoleApp!
+        /// </remarks>
+        public ChatClient(string username, string siteUrl)
+        {
+            // check inputs
+            if (string.IsNullOrWhiteSpace(username))
+                throw new ArgumentNullException(nameof(username));
+            if (string.IsNullOrWhiteSpace(siteUrl))
+                throw new ArgumentNullException(nameof(siteUrl));
+            // save username
+            _username = username;
+            // set the hub URL
+            _hubUrl = siteUrl.TrimEnd('/') + HUBURL;
+        }
+
+        /// <summary>
+        /// Name of the chatter
+        /// </summary>
+        private readonly string _username;
+
+        /// <summary>
+        /// Flag to show if started
+        /// </summary>
+        private bool _started = false;
+
+        /// <summary>
+        /// Start the SignalR client 
+        /// </summary>
+        public async Task StartAsync()
+        {
+            if (!_started)
+            {
+                // create the connection using the .NET SignalR client
+                _hubConnection = new HubConnectionBuilder()
+                    .WithUrl(_hubUrl)
+                    .Build();
+                Console.WriteLine("ChatClient: calling Start()");
+
+                // add handler for receiving messages
+                _hubConnection.On<string, string>(ChatMessageTypes.RECEIVE, (user, message) =>
+                {
+                    HandleReceiveMessage(user, message);
+                });
+
+                // start the connection
+                await _hubConnection.StartAsync();
+
+                Console.WriteLine("ChatClient: Start returned");
+                _started = true;
+
+                // register user on hub to let other clients know they've joined
+                await _hubConnection.SendAsync(ChatMessageTypes.REGISTER, _username);
+            }
+        }
+
+        /// <summary>
+        /// Handle an inbound message from a hub
+        /// </summary>
+        /// <param name="method">event name</param>
+        /// <param name="message">message content</param>
+        private void HandleReceiveMessage(string username, string message)
+        {
+            // raise an event to subscribers
+            MessageReceived?.Invoke(this, new MessageReceivedEventArgs(username, message));
+        }
+
+        /// <summary>
+        /// Event raised when this client receives a message
+        /// </summary>
+        /// <remarks>
+        /// Instance classes should subscribe to this event
+        /// </remarks>
+        public event MessageReceivedEventHandler MessageReceived;
+
+        /// <summary>
+        /// Send a message to the hub
+        /// </summary>
+        /// <param name="message">message to send</param>
+        public async Task SendAsync(string message)
+        {
+            // check we are connected
+            if (!_started)
+                throw new InvalidOperationException("Client not started");
+            // send the message
+            await _hubConnection.SendAsync(ChatMessageTypes.SEND, _username, message);
+        }
+
+        /// <summary>
+        /// Stop the client (if started)
+        /// </summary>
+        public async Task StopAsync()
+        {
+            if (_started)
+            {
+                // disconnect the client
+                await _hubConnection.StopAsync();
+                // There is a bug in the mono/SignalR client that does not
+                // close connections even after stop/dispose
+                // see https://github.com/mono/mono/issues/18628
+                // this means the demo won't show "xxx left the chat" since 
+                // the connections are left open
+                await _hubConnection.DisposeAsync();
+                _hubConnection = null;
+                _started = false;
+            }
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            Console.WriteLine("ChatClient: Disposing");
+            await StopAsync();
+        }
+    }
+
+    /// <summary>
+    /// Delegate for the message handler
+    /// </summary>
+    /// <param name="sender">the SignalRclient instance</param>
+    /// <param name="e">Event args</param>
+    public delegate void MessageReceivedEventHandler(object sender, MessageReceivedEventArgs e);
+
+    /// <summary>
+    /// Message received argument class
+    /// </summary>
+    public class MessageReceivedEventArgs : EventArgs
+    {
+        public MessageReceivedEventArgs(string username, string message)
+        {
+            Username = username;
+            Message = message;
+        }
+
+        /// <summary>
+        /// Name of the message/event
+        /// </summary>
+        public string Username { get; set; }
+
+        /// <summary>
+        /// Message data items
+        /// </summary>
+        public string Message { get; set; }
+
+    }
+}
---
+++ src/Dotnet.DeployTool.UI/Shared/Chat/ChatMessageTypes.cs
@@ -0,0 +1,21 @@
+﻿namespace Dotnet.DeployTool.UI.Shared
+{
+    public static class ChatMessageTypes
+    {
+        /// <summary>
+        /// Event name when a message is received
+        /// </summary>
+        public const string RECEIVE = "ReceiveMessage";
+
+        /// <summary>
+        /// Name of the Hub method to register a new user
+        /// </summary>
+        public const string REGISTER = "Register";
+
+        /// <summary>
+        /// Name of the Hub method to send a message
+        /// </summary>
+        public const string SEND = "SendMessage";
+
+    }
+}
